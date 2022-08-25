e42d942102a67bf2c23775cff2400148b5f4ef92
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2022-08-25 20:14:38 +0200

    Initial commit

---
+++ src/main/java/GUI/Menu.java
@@ -0,0 +1,97 @@
+package GUI;
+
+
+import net.miginfocom.layout.CC;
+import net.miginfocom.swing.MigLayout;
+import static javax.swing.JOptionPane.showMessageDialog;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class Menu extends JFrame {
+
+
+    //App Menu, select mode window
+    public Menu() {
+        super("TasteTester");
+
+        setDefaultLookAndFeelDecorated(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,800);
+        add(menuDistribution());
+
+
+
+
+    }
+    //Base of Distribution of the components
+    private JPanel menuDistribution(){
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new MigLayout("fill,wrap","[]","[][][][]"));
+        buildMiGForm(panel);
+        return panel;
+    }
+
+    //Creation and distribution of the components
+    private void buildMiGForm(JPanel panel){
+
+
+
+
+
+
+        JButton buttonMovie = new JButton();
+        JButton buttonMusic = new JButton();
+        JButton buttonBook = new JButton();
+
+        //Add img icons to the buttons and clear the button background
+        try {
+            URL imgMovie = getClass().getResource("/cinema.png");
+            buttonMovie.setIcon(new ImageIcon(imgMovie));
+            buttonMovie.setBorder(BorderFactory.createEmptyBorder());
+            buttonMovie.setContentAreaFilled(false);
+
+            URL imgMusic = getClass().getResource("/music.png");
+            buttonMusic.setIcon(new ImageIcon(imgMusic));
+            buttonMusic.setBorder(BorderFactory.createEmptyBorder());
+            buttonMusic.setContentAreaFilled(false);
+
+            URL imgBook = getClass().getResource("/book.png");
+            buttonBook.setIcon(new ImageIcon(imgBook));
+            buttonBook.setBorder(BorderFactory.createEmptyBorder());
+            buttonBook.setContentAreaFilled(false);
+
+
+        } catch (Exception ex) {
+            showMessageDialog(null,"Menu Icons Not loaded");
+        }
+
+
+
+        //Adding Font and Title
+        try {
+            Font fontTitle = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("/Maytra.ttf"));
+            JLabel title = new JLabel("TasteTester",SwingConstants.CENTER);
+            title.setFont(fontTitle.deriveFont(65f));
+            title.setForeground(Color.CYAN);
+            title.setOpaque(true);
+            title.setBackground(Color.GRAY);
+
+            panel.add(title,"growy 1,growx");
+
+        } catch (IOException e) {
+            showMessageDialog(null,"Font not found");
+        } catch (FontFormatException e) {
+            showMessageDialog(null,"Font not found");
+        }
+        panel.setBackground(Color.BLACK);
+        panel.add(buttonMovie, "align center");
+        panel.add(buttonBook, "align center");
+        panel.add(buttonMusic, "align center");
+    }
+
+
+}
---
+++ src/main/java/TasteTester.java
@@ -0,0 +1,16 @@
+import GUI.Menu;
+
+import javax.swing.*;
+
+public class TasteTester {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Menu menu = new Menu();
+                menu.setVisible(true);
+            }
+        });
+
+    }
+}
